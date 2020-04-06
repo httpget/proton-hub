2b9c9064c22063e7829f3ce1457707aee14eb9b0
aiden <62084752+httpget@users.noreply.github.com>
2020-04-06 16:21:29 -0400

    Create arsenalgunmod.lua

---
+++ scripts/arsenalgunmod.lua
@@ -0,0 +1,18 @@
+for i,v in pairs(get_gc_objects(true)) do
+	if type(v) == "table" then
+		if rawget(v, "getammo") then
+			--table.foreach(v, warn)
+			v.getammo = function() return 999 end
+			v.getsecondaryammo = function() return 999 end
+			while wait() do
+				v.speedreduction = 5
+				v.recoil = 0
+				v.currentspread = 0
+				v.reloadtime = 0
+				v.currentTargetHealth = 1
+				v.secondaryduration = 0
+				v.mode = "automatic"
+			end
+		end
+	end
+end
